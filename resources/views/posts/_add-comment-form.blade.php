ff97332c230713540e9e79d745276108f30ff5c6
hauja47 <hoangletrunghau01@gmail.com>
2021-11-12 23:38:19 +0700

    Extract add comment and submit button to component

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,32 @@
+@auth
+<x-panel>
+    <form method="POST" action="/posts/{{ $post->slug }}/comments">
+        @csrf
+
+        <header class="flex items-center">
+            <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="40"
+                class="rounded-full">
+            <h2 class="ml-4">some</h2>
+        </header>
+
+        <div class="mt-6">
+            <textarea name="body" placeholder="Your comment" rows="5"
+                class="w-full text-sm focus:outline-none focus:ring" required>
+            </textarea>
+
+            @error('body')
+            <span class="text-xs text-red-500">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200 pt-6">
+            <x-submit-button>Post</x-submit-button>
+        </div>
+    </form>
+</x-panel>
+@else
+<p class="">
+    <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Login</a> to
+    leave a comment.
+</p>
+@endauth
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,41 +49,7 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    @auth
-                        <x-panel>
-                            <form method="POST" action="/posts/{{ $post->slug }}/comments">
-                                @csrf
-
-                                <header class="flex items-center">
-                                    <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="40"
-                                        class="rounded-full">
-                                    <h2 class="ml-4">some</h2>
-                                </header>
-
-                                <div class="mt-6">
-                                    <textarea name="body" placeholder="Your comment" rows="5"
-                                        class="w-full text-sm focus:outline-none focus:ring"
-                                        required>
-                                    </textarea>
-
-                                    @error('body')
-                                        <span class="text-xs text-red-500">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200 pt-6">
-                                    <button
-                                        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
-                                        Post
-                                    </button>
-                                </div>
-                            </form>
-                        </x-panel>
-                    @else
-                        <p class="">
-                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Login</a> to leave a comment.
-                        </p>
-                    @endauth
+                    @include('posts._add-comment-form')
 
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
