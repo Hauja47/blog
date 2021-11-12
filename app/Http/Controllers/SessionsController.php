abd8c63b789d2491b864d3242d8454e2b5902b65
hauja47 <hoangletrunghau01@gmail.com>
2021-11-12 23:31:43 +0700

    Create SessionsController.php

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+        //    'username' => 'required|exists:users,username',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credetials could not be verified.'
+            ]);
+        }
+
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome back!');
+    }
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+}
